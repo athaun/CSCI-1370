98b561d325909923af4ef5f968a1b14323250252
Asher <asher.haun@outlook.com>
2022-10-31 20:42:05 -0500

    Week 3 Lecture

--- lectures/week3/day2.cpp
+++ lectures/week3/day2.cpp
@@ -5,22 +5,22 @@
 #include <iomanip> // output stream maniuplators
 
 int main () {
-    // std::cout << std::showpoint;
+    std::cout << std::showpoint;
     std::cout << std::fixed << std::setprecision(2); // setprecision() without std::fixed will cut off sig figs rather than decimal places
     
-    // std::cout << "C++ int, float, char, int\n";
+    std::cout << "C++ float int char int\n";
 
     float float1;
     int whole2;
     char char3;
     int whole4;
 
-    // std::cin >> float1 >> whole2 >> char3 >> whole4;
-    // std::cout << "float: " << float1 << " int: " << whole2 << " char: '" << char3 << "' int: " << whole4 << "\n";
+    std::cin >> float1 >> whole2 >> char3 >> whole4;
+    std::cout << "float: " << float1 << " int: " << whole2 << " char: '" << char3 << "' int: " << whole4 << "\n";
 
     std::cout << "\n";
 
-    std::cout << "4.493 rounded to 2 decimals = " << round(4.495 * 100.0) / 100.0 << "\n";
+    // std::cout << "4.493 rounded to 2 decimals = " << round(4.495 * 100.0) / 100.0 << "\n";
 
     return 0;
 }
